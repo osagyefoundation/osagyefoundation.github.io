b9ed47494f58e7275a138a8e252f2113096be22d
Michael Affare <aaffare@gmail.com>
2026-01-17 21:36:59 -0500

    images updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,37 +299,42 @@ console.log(
          /* ---------------------------------------------- */
 
     var worksgrid = $("#works-grid"),
-      worksgrid_mode;
+      worksgrid_mode,
+      worksgridStatic =
+        worksgrid.data("static-grid") === true ||
+        worksgrid.data("static-grid") === "true";
 
-    if (worksgrid.hasClass("works-grid-masonry")) {
-      worksgrid_mode = "masonry";
-    } else {
-      worksgrid_mode = "fitRows";
+    if (worksgrid.length && !worksgridStatic) {
+      if (worksgrid.hasClass("works-grid-masonry")) {
+        worksgrid_mode = "masonry";
+      } else {
+        worksgrid_mode = "fitRows";
+      }
+
+      worksgrid.imagesLoaded(function () {
+        worksgrid.isotope({
+          layoutMode: worksgrid_mode,
+          itemSelector: ".work-item",
+        });
+      });
+
+      $("#filters a").click(function () {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+        var selector = $(this).attr("data-filter");
+
+        worksgrid.isotope({
+          filter: selector,
+          animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: false,
+          },
+        });
+
+        return false;
+      });
     }
-
-    worksgrid.imagesLoaded(function () {
-      worksgrid.isotope({
-        layoutMode: worksgrid_mode,
-        itemSelector: ".work-item",
-      });
-    });
-
-    $("#filters a").click(function () {
-      $("#filters .current").removeClass("current");
-      $(this).addClass("current");
-      var selector = $(this).attr("data-filter");
-
-      worksgrid.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: "linear",
-          queue: false,
-        },
-      });
-
-      return false;
-    });
 
     /* ---------------------------------------------- /*
          * Testimonials
